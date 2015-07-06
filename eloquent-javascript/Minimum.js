7827e82f4a15d028408d4d31cb374ed812434f65
Brienna Herold <brienna.herold@gmail.com>
2015-07-06 15:04:26 -0400

    Created "Minimum.js"

---
+++ eloquent-javascript/Minimum.js
@@ -0,0 +1,16 @@
+// Write a function min that takes two arguments and returns their minimum.
+
+function min(x, y) {
+  if (x < y) 
+    return x;
+  else if (x > y)
+    return y;
+}
+
+// Eloquent JavaScript answer:
+function min(a, b) {
+  if (a < b)
+    return a;
+  else
+    return b;
+}
