9a9844d39ab4c2a4c817c73e089205bc6a4472bc
Brienna Herold <brienna.herold@gmail.com>
2015-07-30 16:01:16 -0400

    Added exercise number

--- eloquent-javascript/2.3. Chess board.js
+++ eloquent-javascript/2.3. Chess board.js
@@ -1,4 +1,5 @@
-/* 
+/* 2.3. Chess board
+
 Write a program that creates a string that represents an 8x8 grid, using newline characters to separate lines. 
 At each position of the grid there is either a space or a "#" character. The characters should form a chess 
 board. Passing this string to console.log should show something like this: 
