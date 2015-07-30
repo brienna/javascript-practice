5ea8f79d72fb3e4911ef4d7fc14c39a1b857bcf8
Brienna Herold <brienna.herold@gmail.com>
2015-07-30 16:04:12 -0400

    Added exercise number

--- eloquent-javascript/4.2. Reversing an array.js
+++ eloquent-javascript/4.2. Reversing an array.js
@@ -1,8 +1,8 @@
-/* 
+/* 4.2. Reversing an array
+
 Write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new 
 array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it 
-modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.
-*/
+modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method. */
 
 var reverseArray = function(array) {
   var newArray = [];
