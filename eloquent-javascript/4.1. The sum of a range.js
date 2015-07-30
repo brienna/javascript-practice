8e1e521cb8572c711047806ebbc39b4913a5f048
Brienna Herold <brienna.herold@gmail.com>
2015-07-30 16:04:49 -0400

    Added exercise number

--- eloquent-javascript/4.1. The sum of a range.js
+++ eloquent-javascript/4.1. The sum of a range.js
@@ -1,4 +1,5 @@
-/*
+/* 4.1. The sum of a range
+
 Write a range function that takes two arguments, start and end, and returns an array containing 
 all the numbers from start up to (and including) end.
 
@@ -9,8 +10,7 @@ As a bonus assignment, modify your range function to take an optional third argu
 indicates the “step” value used to build up the array. If no step is given, the array elements 
 go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) 
 should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that 
-range(5, 2, -1) produces [5, 4, 3, 2].
-*/
+range(5, 2, -1) produces [5, 4, 3, 2]. */
 
 function range(start, end, step) {
   step = step || 1;
