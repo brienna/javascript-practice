5a12e3699283ee004e0cc256a78c792fabd0b9bc
Brienna Herold <brienna.herold@gmail.com>
2015-07-30 16:01:44 -0400

    Added exercise number

--- eloquent-javascript/2.2. FizzBuzz.js
+++ eloquent-javascript/2.2. FizzBuzz.js
@@ -1,4 +1,5 @@
-/* 
+/* 2.2. FizzBuzz
+
 Write a program that uses console.log to print all the numbers from 1 to 100, with two
 exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers
 divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program
