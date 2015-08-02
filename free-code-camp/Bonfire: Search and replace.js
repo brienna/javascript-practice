3453f87b7ad1bfc968a32eeeabed9c6071f86438
Brienna Herold <brienna.herold@gmail.com>
2015-08-01 23:00:33 -0400

    Created Bonfire: Search and replace.js

---
+++ free-code-camp/Bonfire: Search and replace.js
@@ -0,0 +1,20 @@
+// Bonfire: Search and replace
+
+// Perform a search and replace on the sentence using the arguments
+// provided and return the new sentence. 
+
+function replace(str, before, after) {
+    var words = str.split(" "); // splits string into array of words
+
+    var replaced = words.map(function(word) {
+        if (word == before) {
+            return after;
+        } else {
+            return word;
+        } 
+    });
+
+    debug(replaced.join(" ")); // prints "A quick brown fox leaped over the lazy dog"
+}
+
+replace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
