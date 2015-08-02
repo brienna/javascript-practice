c59b354da1fc63b4529ecb796cafa1d87c6e4613
Brienna Herold <brienna.herold@gmail.com>
2015-08-01 23:19:10 -0400

    Added given code

--- free-code-camp/Bonfire: Search and replace.js
+++ free-code-camp/Bonfire: Search and replace.js
@@ -3,6 +3,15 @@
 // Perform a search and replace on the sentence using the arguments
 // provided and return the new sentence. 
 
+// Given code: 
+function replace(str, before, after) {
+ return str;
+}
+
+replace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+
+
+// My solution:
 function replace(str, before, after) {
     var words = str.split(" "); // splits string into array of words
 
