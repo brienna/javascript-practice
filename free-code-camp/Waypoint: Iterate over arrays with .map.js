4a05fe3676a1c5c8ab2fe97b99368adbfaa1ab86
Brienna Herold <brienna.herold@gmail.com>
2015-09-04 14:01:48 -0400

    Create Waypoint: Iterate over arrays with .map.js

---
+++ free-code-camp/Waypoint: Iterate over arrays with .map.js
@@ -0,0 +1,12 @@
+// Waypoint: Iterate over arrays with .map
+
+// Use map to add three to each value in the array.
+var array = [1,2,3,4,5];
+// Only change code below this line.
+
+array = array.map(function(n) {
+	return n + 3;
+});
+
+// Only change code above this line.
+(function() {return array;})(); 
