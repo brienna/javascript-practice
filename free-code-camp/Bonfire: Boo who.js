57631188e4fd1bc229aef43adf1c1b806cf9b5f2
Brienna Herold <brienna.herold@gmail.com>
2015-09-20 11:08:15 -0400

    Create Bonfire: Boo who.js

---
+++ free-code-camp/Bonfire: Boo who.js
@@ -0,0 +1,11 @@
+/* Bonfire: Boo who
+
+Check if a value is classified as a boolean primitive. Return true or false. */
+
+function boo(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  if (bool === true || bool === false)
+    return true;
+  else
+    return false; 
+}
