71a4bd5db24f1ee106c3b2917d0d82b62c188860
Brienna Herold <brienna.herold@gmail.com>
2015-09-20 11:09:49 -0400

    Added given code

--- free-code-camp/Bonfire: Boo who.js
+++ free-code-camp/Bonfire: Boo who.js
@@ -1,7 +1,15 @@
-/* Bonfire: Boo who
+// Bonfire: Boo who
 
-Check if a value is classified as a boolean primitive. Return true or false. */
+// Check if a value is classified as a boolean primitive. Return true or false. 
 
+// Given code: 
+function boo(bool) {
+  // What is the new fad diet for ghost developers? The Boolean.
+  return bool;
+}
+
+
+// My solution:
 function boo(bool) {
   // What is the new fad diet for ghost developers? The Boolean.
   if (bool === true || bool === false)
@@ -9,3 +17,5 @@ function boo(bool) {
   else
     return false; 
 }
+
+
