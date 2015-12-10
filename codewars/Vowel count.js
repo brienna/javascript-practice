098985ffb9d4ead407d854e0cf9d000735f7353e
Brienna Herold <brienna.herold@gmail.com>
2015-12-09 20:10:36 -0500

    Create Vowel count.js

---
+++ codewars/Vowel count.js
@@ -0,0 +1,18 @@
+// Return the number (count) of vowels in the given string. We will consider a, e, i, o, and u as vowels for this Kata. 
+
+function getCount(str) {
+  var vowelsCount = 0;
+  var letters = str.split("");
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+  
+  var letter, vowel;
+  for (letter in letters) {
+      for (vowel in vowels) {
+          if (letters[letter] == vowels[vowel]) {
+              vowelsCount++;
+          }
+      }
+  }
+  
+  return vowelsCount;
+}
