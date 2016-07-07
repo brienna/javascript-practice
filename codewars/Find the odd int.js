274d4ba8cae13c412f2b442fdcced9361af43af8
Brienna Herold <brienna.herold@gmail.com>
2016-07-07 19:59:49 -0400

    Create Find the odd int.js

---
+++ codewars/Find the odd int.js
@@ -0,0 +1,28 @@
+/* Find the odd int
+
+Given an array, find the int that appears an odd number of times. 
+There will always be only one integer that appears an odd number of times. */
+
+
+function findOdd(A) {
+	// Count integer frequency 
+	integers = {};
+	for (var i = 0; i < A.length; i++) {
+		if (!integers[A[i]]) {
+			integers[A[i]] = 1;
+		} else {
+			integers[A[i]]++;
+		}
+	}
+	
+	// Find the integer with an odd frequency
+	for (int in integers) {
+		if (integers[int] % 2 != 0) {
+			return Number(int);
+		}
+	}
+}
+
+findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]);  // 5
+findOdd([1,1,1,1,1,1,10,1,1,1,1]);  // 10
+findOdd([1,1,2,-2,5,2,4,4,-1,-2,5]);  // -1
