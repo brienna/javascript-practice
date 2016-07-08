14849e0e957d6694ef6b8cbc4fa53be24bf516d4
Brienna Herold <brienna.herold@gmail.com>
2016-07-07 20:38:38 -0400

    Revise

--- codewars/Find the odd int.js
+++ codewars/Find the odd int.js
@@ -26,3 +26,25 @@ function findOdd(A) {
 findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]);  // 5
 findOdd([1,1,1,1,1,1,10,1,1,1,1]);  // 10
 findOdd([1,1,2,-2,5,2,4,4,-1,-2,5]);  // -1
+
+
+// Revision 1: Use forEach() instead of first for loop
+
+function findOdd(A) {
+	// Count integer frequency 
+	integers = {};
+	A.forEach(function(int, i) {
+		if (!integers[int]) {
+			integers[int] = 1;
+		} else {
+			integers[int]++;
+		}
+	});
+	
+	// Find the integer with an odd frequency
+	for (int in integers) {
+		if (integers[int] % 2 != 0) {
+			return Number(int);
+		}
+	}
+}
